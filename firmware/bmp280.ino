ed835cdf50da753d942f9eb55cc46d06cfcd61e2
Mafer <ladfp19112005@gmail.com>
2025-11-24 19:16:52 -0400

    Adelanto del firmware

--- firmware/bmp280.ino
+++ firmware/bmp280.ino
@@ -1,1 +1,38 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
 
+
+Adafruit_BMP280 bmp;
+
+float TEMPERATURA;
+float PRESION, P0;
+
+void setup() {
+    Serial.begin (9600);
+    Serial.println ("Iniciando:");
+    if ( !bmp.begin ()  )   {
+    Serial.println ("Bmp280 no encontrado !");
+    while (1); 
+}
+P0 = bmp.readPressure()/100;
+}
+
+void loop() {
+    TEMPERATURA = bmp.readTemperature();
+    PRESION = bmp.readPressure()/100;
+
+    Serial.print ("Temperatura: ") ;
+    Serial.print (TEMPERATURA) ;
+    Serial.print (" C ") ;
+
+    Serial.print ("Presion: ") ;
+    Serial.print (PRESION) ;
+    Serial.println (" hPa ") ;
+    
+    Serial.print ("Altitud aprox: ") ;
+    Serial.print (bmp.readAltitude(P0)) ;
+    Serial.println (" m ") ;
+    delay(5000)
+
+}
